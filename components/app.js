706a1db61a4aac0d77b2744f308e801603bca62e
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-07-30 18:01:08 -0500

    added App class

---
+++ components/app.js
@@ -0,0 +1,30 @@
+class App {
+  constructor() {
+    this.handleGetGradesError = this.handleGetGradesError.bind(this);
+    this.handleGetGradesSuccess = this.handleGetGradesSuccess.bind(this);
+  }
+
+  handleGetGradesError(error) {
+    console.error(error);
+  }
+
+  handleGetGradesSuccess(grades) {
+    console.log(grades);
+  }
+
+  getGrades() {
+    $.ajax({
+      method: "GET",
+      url: "https://sgt.lfzprototypes.com/api/grades",
+      headers: {
+        "X-Access-Token": "HsY3ia5l"
+      },
+      error: this.handleGetGradesError,
+      success: this.handleGetGradesSuccess,
+    });
+  }
+
+  start() {
+    this.getGrades();
+  }
+}

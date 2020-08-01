00eddfb6e40b390c470968c578c75d8e3ef06a15
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-07-31 22:30:47 -0500

    Completed Update A Grade

--- components/grade-form.js
+++ components/grade-form.js
@@ -1,12 +1,18 @@
 class GradeForm{
-  constructor(formElement) {
+  constructor(formElement, studentName, courseName, studentGrade, formTitle, submitButton) {
     this.formElement = formElement;
+    this.studentName = studentName;
+    this.courseName = courseName;
+    this.studentGrade = studentGrade;
+    this.formTitle = formTitle;
+    this.submitButton = submitButton;
     this.handleSubmit = this.handleSubmit.bind(this);
     this.formElement.on("submit", this.handleSubmit); // use ".on" because this.formElement is a jQuery selector
   }
 
-  onSubmit(createGrade) {
+  onSubmit(createGrade, updateGrade) {
     this.createGrade = createGrade;
+    this.updateGrade = updateGrade;
   }
 
   handleSubmit(event) {
@@ -15,7 +21,13 @@ class GradeForm{
     var name = formData.get("name");
     var course = formData.get("course");
     var grade = formData.get("grade");
-    this.createGrade(name, course, grade);
+    if(this.formTitle.text() === "Add Grade") {
+      this.createGrade(name, course, grade);
+    } else {
+      this.updateGrade(this.updateId, name, course, grade)
+    }
+    this.formTitle.text("Add Grade");
+    this.submitButton.text("Add");
     event.target.reset();
   }
 }
